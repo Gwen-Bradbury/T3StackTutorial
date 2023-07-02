d1237fefc3f71a1dac127462eacb8723a71b33a3
Gwen Bradbury <charmed_89@msn.com>
2023-07-02 21:54:40 +0100

    seperate SSG helper, full post page

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -2,6 +2,10 @@ import type { GetStaticProps, NextPage } from "next";
 import Image from "next/image";
 import Head from "next/head";
 import { api } from "~/utils/api";
+import { PageLayout } from "~/components/layout";
+import { LoadingPage } from "~/components/loadingSpinner";
+import { PostView } from "~/components/postview";
+import { generateSSGHelper } from "~/server/helpers/ssgHelper";
 
 const ProfileFeed = (props: { userId: string }) => {
   const { data, isLoading } = api.posts.getPostsByUserId.useQuery({
@@ -55,20 +59,8 @@ const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
   );
 };
 
-import { createServerSideHelpers } from "@trpc/react-query/server";
-import superjson from "superjson";
-import { prisma } from "~/server/db";
-import { appRouter } from "~/server/api/root";
-import { PageLayout } from "~/components/layout";
-import { LoadingPage } from "~/components/loadingSpinner";
-import { PostView } from "~/components/postview";
-
 export const getStaticProps: GetStaticProps = async (context) => {
-  const ssg = createServerSideHelpers({
-    router: appRouter,
-    ctx: { prisma, userId: null },
-    transformer: superjson, // optional - adds superjson serialization
-  });
+  const ssg = generateSSGHelper();
 
   const slug = context.params?.slug;
 
